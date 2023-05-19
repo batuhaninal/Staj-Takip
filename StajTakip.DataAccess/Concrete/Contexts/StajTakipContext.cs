e164c0760ab0d536bb8cea67ce59fa0970d3580d
Batuhan Inal <np_batu@hotmail.com>
2023-05-20 01:13:35 +0300

    Conflicts giderildi

--- StajTakip.DataAccess/Concrete/Contexts/StajTakipContext.cs
+++ StajTakip.DataAccess/Concrete/Contexts/StajTakipContext.cs
@@ -17,5 +17,13 @@ namespace StajTakip.DataAccess.Concrete.Contexts
         public DbSet<Signature> Signatures { get; set; }
         public DbSet<BookImage> BookImages { get; set; }
         public DbSet<BookTemplate> BookTemplates { get; set; }
+        public DbSet<InternshipDocument> InternshipDocuments { get; set; }
+
+        public DbSet<User> Users { get; set; }
+        public DbSet<AdminUser> AdminUsers { get; set; }
+        public DbSet<StudentUser> StudentUsers { get; set; }
+        public DbSet<OperationClaim> OperationClaims { get; set; }
+        public DbSet<UserOperationClaim> UserOperationClaims { get; set; }
+
     }
 }
