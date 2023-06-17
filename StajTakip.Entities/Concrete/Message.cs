4e04aab4d6eab4dc1bf7e33164f184231d6fc5e7
Batuhan Inal <np_batu@hotmail.com>
2023-06-17 04:06:34 +0300

    Message entitysi duzenlendi

--- StajTakip.Entities/Concrete/Message.cs
+++ StajTakip.Entities/Concrete/Message.cs
@@ -8,15 +8,16 @@ using System.Threading.Tasks;
 
 namespace StajTakip.Entities.Concrete
 {
-    public class Message : EntityBase
+    public class Message : IEntity
     {
+        public int Id { get; set; }
         public int? SenderUserId { get; set; }
         public virtual User? SenderUser { get; set; }
         public int? ReceiverUserId { get; set; }
         public virtual User? ReceiverUser { get; set; }
         public string Subject { get; set; }
         public string MessageDetail { get; set; }
-        public DateTime MessageDate { get; set; }
+        public DateTime MessageDate { get; set; } = DateTime.Now;
         public bool IsSolved { get; set; }
         public bool IsTeacherRead { get; set; } = false;
         public bool IsCompanyRead { get; set; } = false;
